d32961cbccbaf74619abe847a85ae6e8120ba1fa
RichieBrady <richard.brady@ucdconnect.ie>
2023-05-17 17:52:04 +0100

    Add prod_backend_entrypoint.sh this script waits for the db service to start and runs the django migrate command then runs the gunicorn server

---
+++ backend/entrypoint_scripts/prod_backend_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Wait for the PostgreSQL database to be ready
+until nc -z -v -w30 db 5432; do
+  echo 'Waiting for the database connection...'
+  sleep 1
+done
+
+cd django_api || exit
+# Apply Django database migrations
+python manage.py migrate
+
+# Start the Django development server
+exec gunicorn --bind 0.0.0.0:8000 django_api.wsgi:application

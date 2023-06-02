2c130f56555410dc911077aa0c24a945a448d267
RichieBrady <richard.brady@ucdconnect.ie>
2023-06-02 18:18:08 +0100

    The production entrypoint script will now query the RDS url defined in prod.env and wait for a connection before starting the django server.

--- backend/entrypoint_scripts/prod_backend_entrypoint.sh
+++ backend/entrypoint_scripts/prod_backend_entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Wait for the PostgreSQL database to be ready
-until nc -z -v -w30 db 5432; do
+# $POSTGRES_HOST is the url for the RDS instance. The url is set in backend/prod.env
+until nc -z -v -w30 "$POSTGRES_HOST" 5432; do
   echo 'Waiting for the database connection...'
   sleep 1
 done

424590ce57160485067ac5c58b31d72943b6efbc
RichieBrady <richard.brady@ucdconnect.ie>
2023-06-05 10:11:53 +0100

    Fix formatting with black

--- backend/example_env/gen_django_secret_keys.py
+++ backend/example_env/gen_django_secret_keys.py
@@ -1,5 +1,5 @@
 from django.core.management.utils import get_random_secret_key
 
-print(f'Development Key: {get_random_secret_key()}')
-print(f'Staging Key: {get_random_secret_key()}')
-print(f'Production Key: {get_random_secret_key()}')
+print(f"Development Key: {get_random_secret_key()}")
+print(f"Staging Key: {get_random_secret_key()}")
+print(f"Production Key: {get_random_secret_key()}")
